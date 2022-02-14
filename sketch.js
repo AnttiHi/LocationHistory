4adb243db80f127c58fda0f7e432a715f0f70922
Antti <antti_himanka@hotmail.com>
2022-02-14 11:39:13 +0200

    Added play mode + other improvements

--- sketch.js
+++ sketch.js
@@ -17,7 +17,16 @@ var lastTimestamp;
 var mapstyle = "http://{s}.tile.osm.org/{z}/{x}/{y}.png";
 var dragged = false;
 var accuracy;
+var opacity;
 var heatOn = true;
+var play;
+var playMode = false;
+var timePoint = 0;
+var grain;
+var startTimeIndex;
+var prev = [];
+var timePoints = [];
+var triple = [];
 
 const mappa = new Mappa('Leaflet');
 
@@ -25,7 +34,9 @@ const options = {
   lat: 0,
   lng: 0,
   zoom: 1.5,
-  style: mapstyle
+  style: mapstyle,
+  zoomSnap: 0,
+  boxZoom: true
 }
 
 function preload() {
@@ -41,10 +52,13 @@ function setup() {
   lastTimestamp = data.locations[data.locations.length - 1].timestampMs;
 
   endTime = createSlider(firstTimestamp, lastTimestamp, firstTimestamp, 3600000).size(1000);
-  dotSize = createSlider(5, 20, 10, 0.01);
+  dotSize = createSlider(3, 20, 6, 0.01);
   startTime = createSlider(firstTimestamp, lastTimestamp, firstTimestamp, 3600000).size(1000);
   dotAlpha = createSlider(50, 255, 255, 0.1);
   accuracy = createSlider(1, 20, 1, 1);
+  opacity = createSlider(0, 255, 0, 1);
+  play = createButton('PLAY');
+  grain = createSlider(1, 100, 1, 1);
 
   testMap.onChange(updateMap);
   endTime.changed(updateMap);
@@ -52,6 +66,7 @@ function setup() {
   dotSize.changed(updateMap);
   dotAlpha.changed(updateMap);
   accuracy.changed(updateMap);
+  play.mousePressed(changeMode);
 
   sel = createSelect();
   sel.option('Street map');
@@ -70,22 +85,26 @@ function setup() {
     latitudes.push(lat);
     longitudes.push(lon);
     times.push(time);
+    triple.push({time, lat, lon});
   }
 }
 
 function updateMap() {
   if (dragged) {
     clear();
+    background(255, opacity.value());
     return;
   } else {
     clear();
+    background(255, opacity.value());
     dots = [[]];
-    for (i in times) {
-      if (i % accuracy.value() == 0) {
-        if (times[i] > startTime.value() && times[i] < endTime.value()) {
-          const pix = testMap.latLngToPixel(latitudes[i], longitudes[i]);
-          if (pix.x > 0 && pix.x < windowWidth && pix.y > 0 && pix.y < windowHeight) {
-            if (!inArray(dots, [pix.x, pix.y])) {
+    if (!playMode) {
+      for (i in times) {
+        if (i % accuracy.value() == 0) {
+          if (times[i] > startTime.value() && times[i] < endTime.value()) {
+            const pix = testMap.latLngToPixel(latitudes[i], longitudes[i]);
+            if (pix.x > 0 && pix.x < windowWidth && pix.y > 0 && pix.y < windowHeight) {
+              //if (!inArray(dots, [pix.x, pix.y])) {
               if (heatOn) {
                 heat = map(data.locations[i].timestampMs, startTime.value(), endTime.value(), 0, 220);
                 fill(heat, 70, (220 - heat), dotAlpha.value());
@@ -94,8 +113,9 @@ function updateMap() {
               }
               ellipse(pix.x, pix.y, dotSize.value());
               noStroke();
-              dots.push([pix.x, pix.y]);
+              //dots.push([pix.x, pix.y]);
             }
+            //}
           }
         }
       }
@@ -145,4 +165,56 @@ function mouseDragged() {
 function mouseReleased() {
   dragged = false;
   updateMap();
+}
+
+function changeMode() {
+  if (playMode == true) {
+    playMode = false;
+  } else {
+    startTimeIndex = 0;
+    while (times[startTimeIndex] < startTime.value()) {
+      startTimeIndex += 1;
+    }
+    timePoint = startTimeIndex;
+    playMode = true;
+    prev = [0, 0];
+    timePoints = [timePoint];
+  }
+}
+
+function draw() {
+  if (playMode) {
+    clear();
+    background(255, opacity.value());
+    for (let x in timePoints) {
+      dot = timePoints[x];
+      if (times[dot] < endTime.value()) {
+        const pix = testMap.latLngToPixel(latitudes[dot], longitudes[dot]);
+          if (pix.x - prev[0] != 0 && pix.y - prev[1] != 0) {
+            if (heatOn) {
+              heat = map(x, 0, (timePoints.length - 1), 0, 220);
+              fill(heat, 70, (220 - heat), Math.max(heat, 100));
+            } else {
+              fill(255, 0, 255, dotAlpha.value());
+            }
+            ellipse(pix.x, pix.y, dotSize.value());
+            noStroke();
+            prev[0] = pix.x;
+            prev[1] = pix.y;
+          }
+      } else {
+        playMode = false;
+      }
+    }
+    timePoint += grain.value();
+    timePoints.push(timePoint);
+
+    var currentFormatted = new Date(times[timePoint]);
+    var currentDate = new Date(currentFormatted);
+
+    var currentResult = currentDate.toLocaleString('fi');
+    textSize(32);
+    fill(0, 0, 0, 120);
+    text(currentResult, 100, 30);
+  }
 }
